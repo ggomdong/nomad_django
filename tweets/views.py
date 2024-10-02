48ca95a6d4a701d8df4e43305b274b93dc89c538
ggomdong <ggomdong@gmail.com>
2024-10-02 22:44:24 +0900

    assignment 27

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Tweet
 
-# Create your views here.
+
+def see_all_tweets(request):
+    tweets = Tweet.objects.all()
+
+    context = {'title': 'All Tweets', 'tweets': tweets,}
+
+    return render(request, "all_tweets.html", context)
